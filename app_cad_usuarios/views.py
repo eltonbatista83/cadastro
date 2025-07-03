c88ab35cc36ea7974b3c31c8fe27191621e0501a
Elton Medeiros Batista <eltonbatista83@gmail.com>
2025-07-03 18:23:53 -0300

    Primeiro commit

---
+++ app_cad_usuarios/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Usuario
+
+def home (request):
+    return render(request,'usuarios/home.html')
+
+def usuarios(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.email = request.POST.get('email')
+    novo_usuario.telefone = request.POST.get('telefone')
+    novo_usuario.save()
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+
+    }
+    return render(request,'usuarios/usuarios.html',usuarios)
+
+    
